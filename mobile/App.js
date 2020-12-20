ef54f3836aa481c4ff816b4c64cd9b25dd01a512
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-12-20 06:55:25 -0300

    implemented Root for Toast

--- mobile/App.js
+++ mobile/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { Root } from "native-base";
 import MainStack from "./src/stacks/MainStack.js";
 
 //import store from "./src/store/store";
@@ -10,10 +10,12 @@ import MainStack from "./src/stacks/MainStack.js";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="light" />
-      {/*<Provider store={store}></Provider>*/}
-      <MainStack />
-    </NavigationContainer>
+    <Root>
+      <NavigationContainer>
+        <StatusBar style="light" />
+        {/*<Provider store={store}></Provider>*/}
+        <MainStack />
+      </NavigationContainer>
+    </Root>
   );
 }
