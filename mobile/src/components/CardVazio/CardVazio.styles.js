75ebf24de96b8f3d7576d70176f426ccd382b9ae
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-12-20 07:05:34 -0300

    included CardVazio.styles.js

---
+++ mobile/src/components/CardVazio/CardVazio.styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+import { colors } from '../../service/colors';
+
+export const View = styled.View`
+    justify-content: center;
+    align-items: center;
+    margin-top: 50;
+    width: ${Dimensions.get("screen").width};
+`;
+
+export const Text = styled.Text`
+    margin-top: 10;
+    font-size: 17;
+    color: ${colors.gray};
+    margin-start: 10;
+    align-self: center;
+`;
