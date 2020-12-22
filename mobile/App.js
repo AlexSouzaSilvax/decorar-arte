1509833f7dbe1898ef442016420d0b81bb62c644
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-12-22 01:58:36 -0300

    included import Routes old version

--- mobile/App.js
+++ mobile/App.js
@@ -4,6 +4,7 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { Root } from "native-base";
 import MainStack from "./src/stacks/MainStack.js";
+import Routes from "./src/Routes.js";
 
 //import store from "./src/store/store";
 //import { Provider } from "react-redux";
@@ -14,7 +15,8 @@ export default function App() {
       <NavigationContainer>
         <StatusBar style="light" />
         {/*<Provider store={store}></Provider>*/}
-        <MainStack />
+        {/*<MainStack />*/}
+        <Routes />
       </NavigationContainer>
     </Root>
   );
