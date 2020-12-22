9034c5cd9ded03efb71e2b6bead88ae23178c2cf
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-12-22 02:02:14 -0300

    included btnMenu

--- mobile/src/components/Header/Header.jsx
+++ mobile/src/components/Header/Header.jsx
@@ -3,9 +3,15 @@ import { Wrapper, Title, BtnFiltro } from "./Header.styles";
 import { Icon } from 'native-base';
 import { colors } from '../../service/colors';
 
-export default function Header({ title, onPressFiltro, onPressVoltar, styleTitle }) {
+export default function Header({ title, onPressFiltro, onPressVoltar, styleTitle, onPressMenu }) {
   return (
     <Wrapper>
+      {/*BTNMenu*/}
+      { onPressMenu ?
+        <BtnFiltro onPress={onPressMenu}>
+          <Icon name="menu" type="Entypo" style={{ color: colors.white, alignSelf: "flex-start", paddingStart: 25 }} />
+        </BtnFiltro>
+        : <></>}
       {/*BTNVOLTAR*/}
       { onPressVoltar ?
         <BtnFiltro onPress={onPressVoltar}>
@@ -15,7 +21,7 @@ export default function Header({ title, onPressFiltro, onPressVoltar, styleTitle
       <Title style={styleTitle} >{title}</Title>
       { onPressFiltro ?
         <BtnFiltro onPress={onPressFiltro}>
-          <Icon name="filter" type="AntDesign" style={{ color: colors.white, alignSelf: "flex-end", paddingEnd: 20 }} />
+          <Icon name="filter" type="AntDesign" style={{ color: colors.white, alignSelf: "flex-end", paddingEnd: 25 }} />
         </BtnFiltro>
         : <></>}
     </Wrapper>
