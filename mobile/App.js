4ac79b846829a7f1ce175102775d51edd8edf89e
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2020-12-28 15:38:40 -0300

    migration expo for react-native

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { Root } from "native-base";
-import MainStack from "./src/stacks/MainStack.js";
 import Routes from "./src/Routes.js";
-
-//import store from "./src/store/store";
-//import { Provider } from "react-redux";
 
 export default function App() {
   return (
     <Root>
       <NavigationContainer>
-        <StatusBar style="light" />
-        {/*<Provider store={store}></Provider>*/}
-        {/*<MainStack />*/}
+        <StatusBar style="light" /> 
         <Routes />
       </NavigationContainer>
     </Root>
--- mobile/src/Routes.js
+++ mobile/src/Routes.js
@@ -1,13 +1,10 @@
-
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import Login from "./screens/Login";
 import Home from "./screens/Home";
 import DetalheHome from "./screens/DetalheHome";
 
 const Routes = createAppContainer(
     createSwitchNavigator({
-        //Login,
         Home,
         DetalheHome
     })
