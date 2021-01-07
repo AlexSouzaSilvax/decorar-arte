2e5b1e85214998ebe6bb3f1ab57d96c92eff8510
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2021-01-06 22:45:09 -0300

    altered import repository

--- backend/src/main/java/com/decorararte/backend/BackendApplication.java
+++ backend/src/main/java/com/decorararte/backend/BackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.decorarte.backend.Repository") // pacote onde está meu repository
+@EnableJpaRepositories(basePackages = "com.decorararte.backend.Repository") // pacote onde está meu repository
 @SpringBootApplication
 public class BackendApplication {
 
